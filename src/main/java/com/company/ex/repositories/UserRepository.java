2688684ad1f7d6ef8393980175b5552de422c1c3
IannNS <iannnschmith@gmail.com>
2025-12-10 17:56:04 -0300

    Creating service layer and making component registration

--- src/main/java/com/company/ex/repositories/UserRepository.java
+++ src/main/java/com/company/ex/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package com.company.ex.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.company.ex.entities.User;
 
+// Optional
+@Repository 
 public interface UserRepository extends JpaRepository<User, Long>{
-	
 	
 }
