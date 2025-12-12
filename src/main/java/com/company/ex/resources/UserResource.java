eacdc7845dc8d17e1437dbae485c9030a9fdf3b9
IannNS <iannnschmith@gmail.com>
2025-12-12 15:11:49 -0300

    Creating insert User method

--- src/main/java/com/company/ex/resources/UserResource.java
+++ src/main/java/com/company/ex/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.company.ex.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.company.ex.entities.User;
 import com.company.ex.services.UserService;
@@ -19,7 +23,6 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	
 	@GetMapping 
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
@@ -27,8 +30,17 @@ public class UserResource {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		// .created() (201 code) is a HTTP that needs a URI obj. 
+		// It needs a header (location) that contains the address for the resource you posted 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
